1ca765cdbbba828fd539b2ae5f15a3b181a922c8
danthb <danthb@hotmail.com>
2022-09-17 17:53:29 -0500

    refactor:  (before) extract func & rename variabls

--- chapter-01/src/app.ts
+++ chapter-01/src/app.ts
@@ -36,11 +36,11 @@ function amountFor(aPerformance: Performance) {
   return result;
 }
 
-function volumeCreditsFor(perf: Performance) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
-  if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-  return volumeCredits;
+function volumeCreditsFor(aPerformance: Performance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+  return result;
 }
 
 
