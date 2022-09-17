0f58bf0cd8ab548f5c9c99a27d6a5a1d8a3ee4e9
danthb <danthb@hotmail.com>
2022-09-17 13:54:04 -0500

    refactor: rename variables to more readable

--- chapter-01/src/app.ts
+++ chapter-01/src/app.ts
@@ -2,26 +2,26 @@ import invoices from "../data/invoices.json";
 import plays from "../data/plays.json";
 import { Invoice } from "./types";
 
-function amountFor(perf: any, play: any) {
-  let thisAmount = 0;
+function amountFor(aPerformance: any, play: any) {
+  let result = 0;
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 function statement(invoice:Invoice, plays: any) {
@@ -33,17 +33,17 @@ function statement(invoice:Invoice, plays: any) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount= amountFor(perf, play);
+  for (let aPerformance of invoice.performances) {
+    const play = plays[aPerformance.playID];
+    let thisAmount= amountFor(aPerformance, play);
     // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
     
     // print line for this order
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
+      aPerformance.audience
     } seats)\n`;
     totalAmount += thisAmount;
   }
